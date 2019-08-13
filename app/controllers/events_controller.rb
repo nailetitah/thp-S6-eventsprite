44de795ec8e7333ee2b539593a7d4373c04b1d34
ntitah <naile.titah@gmail.com>
2019-08-13 15:42:23 +0200

    Events and user pages, possibilty to create events and new user fields

---
+++ app/controllers/events_controller.rb
@@ -0,0 +1,60 @@
+class EventsController < ApplicationController
+# before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
+
+  def index
+    @events = Event.all.order("created_at DESC")
+  end
+
+  def show 
+    @event = Event.find(params[:id])
+  end
+
+
+  def new
+    @event = Event.new
+  end
+
+  def create
+    @event = Event.new(event_params)
+    if @event.save
+      flash[:success] = "L'événément a été ajouté."
+      redirect_to @event
+    else
+      render 'new'
+    end 
+  end
+
+  def edit
+    @event = Event.find(params[:id])
+  end
+
+  def update
+    @event = Event.find(params[:id])
+    if @event.update(event_params)
+      flash[:success] = "L'événément a été modifié."
+      redirect_to @event
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @event = Event.find(params[:id])
+    @event.destroy
+    flash[:success] = "L'événément a été supprimé."
+    redirect_to root_path
+  end
+
+  private
+
+  def event_params
+    params.require(:event).permit(:start_date, :duration, :title, :description, :price, :location)
+  end
+
+  def find_event
+    @event = Event.find(params[:id])
+  end
+
+end
+
+
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
-  root 'pages#index'
-  get 'pages/secret' => 'pages#secret'
+  root 'events#index'
+  resources :events, controller: 'events'
+  resources :users, only: [:show]
 end
